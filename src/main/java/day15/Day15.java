f21b22b73273dc2ff7d0f26e8c1804380ac078fa
Craig.Schwarzwald@gmail.com <Craig.Schwarzwald@gmail.com>
2024-12-15 11:57:26 -0500

    Day15 Part 2. Still not getting the correct answer.  Not sure why.

--- src/main/java/day15/Day15.java
+++ src/main/java/day15/Day15.java
@@ -21,6 +21,7 @@ public class Day15 {
 	private int instructionPos;
 	public Map map;
 	public Coordinate robotPosition;
+	int totalWalls;
 
 	public Day15() {
 		URL fileName = getClass().getResource("Input.txt");
@@ -29,12 +30,14 @@ public class Day15 {
 		intructions = new File(fileName2.getPath());
 		populateInput();
 		instructionPos = 0;
+		totalWalls = 0;
 	}
 	public Day15(File file, File instructionFile) {
 		instructionsList = new ArrayList<Directions>();
 		setFileToUse(file, instructionFile);
 		populateInput();
 		instructionPos = 0;
+		totalWalls = 0;
 	}
 
 	protected void setFileToUse(File file, File instructionFile) {
@@ -71,39 +74,42 @@ public class Day15 {
 		}
 	}
 	
-	public Integer getDistanceFromRobot(char c, Directions dir) {
-		int row = (int) robotPosition.getY();
-		int col = (int) robotPosition.getX();
+	public Integer getDistanceFromPoint(int row, int col, char c, Directions dir) {
+		int count = 0;
 		switch (dir) {
 		case UP:
 			while (row>0) {
 				row--;
+				count++;
 				if(map.getArea().get(row).get(col)==c) {
-					return (int) (robotPosition.getY()-row);
+					return count;
 				}
 			}
 			break;
 		case DOWN:
 			while (row<map.getArea().size()-1) {
 				row++;
+				count++;
 				if(map.getArea().get(row).get(col)==c) {
-					return (int) (row-robotPosition.getY());
+					return count;
 				}
 			}
 			break;
 		case LEFT:
 			while (col>0) {
 				col--;
+				count++;
 				if(map.getArea().get(row).get(col)==c) {
-					return (int) (robotPosition.getX()-col);
+					return count;
 				}
 			}
 			break;
 		case RIGHT:
 			while (col<map.getArea().get(row).size()-1) {
 				col++;
+				count++;
 				if(map.getArea().get(row).get(col)==c) {
-					return (int) (col-robotPosition.getX());
+					return count;
 				}
 			}
 			break;
@@ -113,30 +119,68 @@ public class Day15 {
 		return Integer.MAX_VALUE;
 	}
 	
+	public Integer getDistanceFromRobot(char c, Directions dir) {
+		return getDistanceFromPoint((int)robotPosition.getY(), (int)robotPosition.getX(), c, dir);
+	}
+	
 	public boolean canMove(Directions dir) {
 		return (getDistanceFromRobot('.', dir)<getDistanceFromRobot('#', dir));
 	}
 	
 	public void moveNext() {
 		Directions curDir = instructionsList.get(instructionPos);
+//		System.out.println("Moving: " + curDir);
+		String prior = map.toString();
 		int row = (int) robotPosition.getY();
 		int col = (int) robotPosition.getX();
 		if(canMove(curDir)) {
 			int nextDotAway = getDistanceFromRobot('.', curDir);
 			switch (curDir) {
 			case UP:
-				for(int x=row-nextDotAway; x<row; x++) {
-					map.updateArea(x, col, map.getArea().get(x+1).get(col));
+				if(map.getArea().get(row-1).get(col)=='[') {
+					if(performBlockMoveUpOnLeft(row-1, col)) {						
+						map.updateArea(row-1, col, '@');
+						map.updateArea(row-1, col+1, '.');
+						map.updateArea(row, col, '.');//Prior robot space is now free
+						robotPosition = new Coordinate(col, row-1);
+					}
+				} else if(map.getArea().get(row-1).get(col)==']') {
+					if(performBlockMoveUpOnRight(row-1, col)) {						
+						map.updateArea(row-1, col-1, '.');
+						map.updateArea(row-1, col, '@');
+						map.updateArea(row, col, '.');//Prior robot space is now free
+						robotPosition = new Coordinate(col, row-1);
+					}
+				}else {
+					for(int x=row-nextDotAway; x<row; x++) {
+						map.updateArea(x, col, map.getArea().get(x+1).get(col));
+					}
+					map.updateArea(row, col, '.');//Prior robot space is now free
+					robotPosition = new Coordinate(col, row-1);
 				}
-				map.updateArea(row, col, '.');//Prior robot space is now free
-				robotPosition = new Coordinate(col, row-1);
 				break;
 			case DOWN:
-				for(int x=row+nextDotAway; x>row; x--) {
-					map.updateArea(x, col, map.getArea().get(x-1).get(col));
+				if(map.getArea().get(row+1).get(col)=='[') {
+					if(performBlockMoveDownOnLeft(row+1, col)) {						
+						map.updateArea(row+1, col, '@');
+						map.updateArea(row+1, col+1, '.');
+						map.updateArea(row, col, '.');//Prior robot space is now free
+						robotPosition = new Coordinate(col, row+1);
+					}
+				} else if(map.getArea().get(row+1).get(col)==']') {
+					if(performBlockMoveDownOnRight(row+1, col)) {						
+						map.updateArea(row+1, col-1, '.');
+						map.updateArea(row+1, col, '@');
+						map.updateArea(row, col, '.');//Prior robot space is now free
+						robotPosition = new Coordinate(col, row+1);
+					}
+				}else {
+					for(int x=row+nextDotAway; x>row; x--) {
+						map.updateArea(x, col, map.getArea().get(x-1).get(col));
+					}
+					map.updateArea(row, col, '.');//Prior robot space is now free
+					robotPosition = new Coordinate(col, row+1);
 				}
-				map.updateArea(row, col, '.');//Prior robot space is now free
-				robotPosition = new Coordinate(col, row+1);
 				break;
 			case LEFT:
 				for(int x=col-nextDotAway; x<col; x++) {
@@ -157,10 +201,140 @@ public class Day15 {
 			}
 		}
 		instructionPos++;
+//		System.out.println(map);
+		if(map.toString().contains("[.") || map.toString().contains(".]") || 
+				map.toString().contains("]]") ||  map.toString().contains("[[") ||
+				isWallMissing()) {
+			System.out.println("We have an issue!");
+			System.out.println("Prior:");
+			System.out.println(prior);
+			System.out.println("Then moving: " + curDir);
+			System.err.println(map);
+			System.out.println("End");
+		}
+	}
+	
+	private boolean isWallMissing() {
+		int curWallCount = 0;
+		for(int y=0; y<map.getArea().size(); y++) {
+			for(int x=0; x<map.getArea().get(y).size(); x++) {
+				char curChar = map.getArea().get(y).get(x);
+				if(curChar=='#') {
+					curWallCount++;
+				}
+			}
+		}
+		return curWallCount!=totalWalls;
+	}
+	private boolean performBlockMoveUpOnLeft(int row, int col) {
+		boolean shouldUpdate = true;
+		if(row>0) {			
+			if((map.getArea().get(row-1).get(col)=='#') || (map.getArea().get(row-1).get(col+1)=='#')) {
+				return false;
+			}
+			if(map.getArea().get(row-1).get(col)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnLeft(row-1, col);
+			} else if(map.getArea().get(row-1).get(col)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnRight(row-1, col);
+			}
+			if(map.getArea().get(row-1).get(col+1)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnLeft(row-1, col+1);
+			} else if(map.getArea().get(row-1).get(col+1)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnRight(row-1, col+1);
+			}
+			if(shouldUpdate) {				
+				map.updateArea(row-1, col, '[');
+				map.updateArea(row-1, col+1, ']');
+				map.updateArea(row, col, '.');
+				map.updateArea(row, col+1, '.');
+				return true;
+			}
+		}
+		return false;
+	}
+	private boolean performBlockMoveUpOnRight(int row, int col) {
+		boolean shouldUpdate = true;
+		if(row>0) {			
+			if((map.getArea().get(row-1).get(col)=='#') || (map.getArea().get(row-1).get(col-1)=='#')) {
+				return false;
+			}		
+			if(map.getArea().get(row-1).get(col)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnLeft(row-1, col);
+			} else if(map.getArea().get(row-1).get(col)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnRight(row-1, col);
+			}
+			if(map.getArea().get(row-1).get(col-1)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnLeft(row-1, col-1);
+			} else if(map.getArea().get(row-1).get(col-1)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveUpOnRight(row-1, col-1);
+			}
+			if(shouldUpdate) {					
+				map.updateArea(row-1, col, ']');
+				map.updateArea(row-1, col-1, '[');
+				map.updateArea(row, col, '.');
+				map.updateArea(row, col-1, '.');
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean performBlockMoveDownOnLeft(int row, int col) {
+		boolean shouldUpdate = true;
+		if(row<map.getArea().size()-1) {				
+			if((map.getArea().get(row+1).get(col)=='#') || (map.getArea().get(row+1).get(col+1)=='#')) {
+				return false;
+			}
+			if(map.getArea().get(row+1).get(col)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnLeft(row+1, col);
+			} else if(map.getArea().get(row+1).get(col)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnRight(row+1, col);
+			}
+			if(map.getArea().get(row+1).get(col+1)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnLeft(row+1, col+1);
+			} else if(map.getArea().get(row+1).get(col+1)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnRight(row+1, col+1);
+			}
+			if(shouldUpdate) {					
+				map.updateArea(row+1, col, '[');
+				map.updateArea(row+1, col+1, ']');
+				map.updateArea(row, col, '.');
+				map.updateArea(row, col+1, '.');
+				return true;
+			}
+		}
+		return false;
+	}
+	private boolean performBlockMoveDownOnRight(int row, int col) {
+		boolean shouldUpdate = true;
+		if(row<map.getArea().size()-1) {				
+			if((map.getArea().get(row+1).get(col)=='#') || (map.getArea().get(row+1).get(col-1)=='#')) {
+				return false;
+			}			
+			if(map.getArea().get(row+1).get(col)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnLeft(row+1, col);
+			} else if(map.getArea().get(row+1).get(col)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnRight(row+1, col);
+			}
+			if(map.getArea().get(row+1).get(col-1)=='[') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnLeft(row+1, col-1);
+			} else if(map.getArea().get(row+1).get(col-1)==']') {
+				shouldUpdate = shouldUpdate && performBlockMoveDownOnRight(row+1, col-1);
+			}
+			if(shouldUpdate) {				
+				map.updateArea(row+1, col, ']');
+				map.updateArea(row+1, col-1, '[');
+				map.updateArea(row, col, '.');
+				map.updateArea(row, col-1, '.');
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public void moveAll() {
 		for(int x=0; x<instructionsList.size(); x++) {
+			System.out.println("Move " + x + "/" + instructionsList.size());
 			moveNext();
 		}
 	}
@@ -169,7 +343,7 @@ public class Day15 {
 		long sum = 0;
 		for(int row=0; row<map.getArea().size(); row++) {
 			for(int col=0; col<map.getArea().get(row).size(); col++) {
-				if(map.getArea().get(row).get(col)=='O') {
+				if(map.getArea().get(row).get(col)=='O' || map.getArea().get(row).get(col)=='[') {
 					sum+=getGPS(row, col);
 				}
 			}
@@ -180,4 +354,84 @@ public class Day15 {
 	private long getGPS(int row, int col) {
 		return 100*row + col;
 	}
+	
+	public void adjustMapForPart2() {
+		ArrayList<ArrayList<Character>> newArea = new ArrayList<ArrayList<Character>>();
+		for(int row=0; row<map.getArea().size(); row++) {
+			ArrayList<Character> newRow = new ArrayList<Character>();
+			for(int col=0; col<map.getArea().get(row).size(); col++) {
+				char curChar = map.getArea().get(row).get(col);
+				if(curChar=='#') {
+					newRow.add('#');
+					newRow.add('#');
+					totalWalls+=2;
+				}
+				else if(curChar=='O') {
+					newRow.add('[');
+					newRow.add(']');
+				}
+				else if(curChar=='.') {
+					newRow.add('.');
+					newRow.add('.');
+				}
+				else if(curChar=='@') {
+					newRow.add('@');
+					newRow.add('.');
+				}
+				else {
+					throw new RuntimeException("Invalid map char: " + curChar);
+				}
+			}
+			newArea.add(newRow);
+		}
+		map.setArea(newArea);
+		assignRobot();
+	}
+	
+	public void assignRobot() {
+		for(int row=0; row<map.getArea().size(); row++) {
+			for(int col=0; col<map.getArea().get(row).size(); col++) {
+				char curChar = map.getArea().get(row).get(col);
+				if(curChar=='@') {
+					robotPosition = new Coordinate(col, row);
+				}
+			}
+		}
+	}
+	
+	public boolean canMove(int row, int col, Directions dir) {
+		char curChar;
+		if(dir==LEFT || dir==RIGHT) {
+			return canMove(dir);
+		} else {
+			if(dir==UP) {
+				curChar = map.getArea().get(row-1).get(col);
+			}else {
+				curChar = map.getArea().get(row+1).get(col);
+			}
+		}
+		
+		if(curChar=='.') {
+			return true;
+		}
+		if(curChar=='#') {
+			return false;
+		}
+		if(curChar=='[') {
+			if(dir==UP) {
+				return canMove(row-1, col, dir) && canMove(row-1, col+1, dir);
+			} else {
+				return canMove(row+1, col, dir) && canMove(row+1, col+1, dir);
+			}
+		} else if(curChar==']') {
+			if(dir==UP) {
+				return canMove(row-1, col, dir) && canMove(row-1, col-1, dir);
+			} else {
+				return canMove(row+1, col, dir) && canMove(row+1, col-1, dir);
+			}
+		} else {
+			throw new RuntimeException("Invalid curChar: " + curChar);
+		}
+	}
+	
 }
